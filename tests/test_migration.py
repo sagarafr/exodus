7170822cbeda6ba2b47746944b74ed2634f614a6
Marian Gappa <marian.gappa@corp.ovh.com>
2017-04-21 18:19:02 +0200

    test(migration): migration function

---
+++ tests/test_migration.py
@@ -0,0 +1,23 @@
+from connections.glance_connection import OVHGlanceConnection
+from connections.openstack_connection import OVHOpenStackConnection
+from migration.migration import migration
+
+
+def main():
+    ovh_openstack_connection = OVHOpenStackConnection()
+    ovh_openstack_connection.ask_credentials()
+    ovh_openstack_connection.connect()
+
+    credentials = dict(ovh_openstack_connection.authentication.credentials_to_dict())
+    credentials.update({'token': ovh_openstack_connection.token})
+    ovh_glance_connection_source = OVHGlanceConnection(**credentials)
+    ovh_glance_connection_source.region_name = "GRA3"
+    ovh_glance_connection_source.connect()
+    ovh_glance_connection_destination = OVHGlanceConnection(**credentials)
+    ovh_glance_connection_destination.region_name = "BHS3"
+    ovh_glance_connection_destination.connect()
+    migration(ovh_glance_connection_source, ovh_glance_connection_destination,
+              input("Snapshot name: "), input("Server name: "), input("Disk format: "), input("Container format: "))
+
+if __name__ == '__main__':
+    main()
