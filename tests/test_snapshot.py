40167e684d8bdcfd91098d3c8122e90ca67b4b0e
Marian Gappa <marian.gappa@corp.ovh.com>
2017-04-21 18:18:09 +0200

    test(migration): snapshot function

---
+++ tests/test_snapshot.py
@@ -0,0 +1,13 @@
+from connections.nova_connection import OVHNovaConnection
+from migration.snapshot import make_snapshot
+
+
+def main():
+    ovh_nova_connection = OVHNovaConnection()
+    ovh_nova_connection.authentication.region_name = "GRA3"
+    ovh_nova_connection.ask_credentials()
+    ovh_nova_connection.connect()
+    make_snapshot(ovh_nova_connection, "arch_openstack", "refactor_src")
+
+if __name__ == '__main__':
+    main()
