8ad44b0e47d5b39055a0c53e93da7c0dbec1bd7b
Marian Gappa <marian.gappa@corp.ovh.com>
2017-04-21 15:24:05 +0200

    test(connection): Nova Add nova test

---
+++ tests/test_nova_connection.py
@@ -0,0 +1,13 @@
+from connections.nova_connection import OVHNovaConnection
+
+
+def main():
+    ovh_nova_connection = OVHNovaConnection()
+    ovh_nova_connection.ask_credentials()
+    ovh_nova_connection.connect()
+    for server in ovh_nova_connection.connection.servers.list():
+        print(server)
+
+
+if __name__ == '__main__':
+    main()
