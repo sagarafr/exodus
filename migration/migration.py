63303b0ca6f4fe975c1bc2c4f32f5838e87f9ee5
Marian Gappa <marian.gappa@corp.ovh.com>
2017-04-21 18:18:39 +0200

    feat(migration): Add migration function

---
+++ migration/migration.py
@@ -0,0 +1,41 @@
+from connections.glance_connection import GlanceConnection
+from utils.get_ids import get_snapshot_id_from_glance
+from tempfile import NamedTemporaryFile
+from os import mkfifo
+from os import fork
+from os import unlink
+from os import wait
+
+
+def migration(glance_source: GlanceConnection, glance_destination: GlanceConnection,
+              snapshot_name: str, server_name_dest: str,
+              disk_format: str, container_format: str):
+    snapshot_uuid_list = get_snapshot_id_from_glance(glance_source, snapshot_name)
+    if len(snapshot_uuid_list) == 0:
+        raise ValueError("Couldn't find the following snapshot: " + snapshot_name)
+
+    # TODO lets make a user choice
+    snapshot_uuid = snapshot_uuid_list[0]
+    data = glance_source.connection.images.data(snapshot_uuid)
+    pipe_filename = NamedTemporaryFile().name
+    image = glance_destination.connection.images.create(name=server_name_dest,
+                                                        disk_format=disk_format,
+                                                        container_format=container_format)
+    image_uuid = str(image.get('id'))
+
+    if data.wrapped is not None:
+        mkfifo(pipe_filename)
+        if fork() != 0:
+            print("parent")
+            reading_pipe = open(pipe_filename, 'rb')
+            glance_destination.connection.images.upload(image_uuid, reading_pipe)
+            reading_pipe.close()
+            unlink(pipe_filename)
+            wait()
+        else:
+            print("child")
+            writing_pipe = open(pipe_filename, 'wb')
+            for binary_data in data.wrapped:
+                writing_pipe.write(binary_data)
+            writing_pipe.close()
+            exit(0)
