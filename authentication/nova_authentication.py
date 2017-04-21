0fb8361e027afa677d532eaca85720913e0325a5
Marian Gappa <marian.gappa@corp.ovh.com>
2017-04-21 18:19:50 +0200

    fix(authentication): nova Add region_name for nova connection

--- authentication/nova_authentication.py
+++ authentication/nova_authentication.py
@@ -11,6 +11,8 @@ class OVHNovaAuthentication(NovaAuthentication, OVHAuthentication):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self["version"] = "2"
+        if 'region_name' not in self:
+            self['region_name'] = "SBG3"
 
     @property
     def version(self):
@@ -19,3 +21,11 @@ class OVHNovaAuthentication(NovaAuthentication, OVHAuthentication):
     @version.setter
     def version(self, value: str):
         self["version"] = value
+
+    @property
+    def region_name(self):
+        return self['region_name']
+
+    @region_name.setter
+    def region_name(self, value):
+        self['region_name'] = value
