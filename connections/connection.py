585c1152fe0053ed5b7949a5c11995fb5804c2fa
Marian Gappa <marian.gappa@corp.ovh.com>
2017-04-24 11:02:41 +0200

    refactor(connections): constructor and credentials Constructor explicit call Remove all openstack_project reference

--- connections/connection.py
+++ connections/connection.py
@@ -1,8 +1,9 @@
-from connections.openstack_project import OpenStackProject
+from authentication.authentication import Authentication
 
 
 class Connection(object):
     def __init__(self):
+        super().__init__()
         self._authentication = None
         self._connection = None
 
@@ -25,8 +26,8 @@ class Connection(object):
     def connect(self):
         raise NotImplemented("Must implement connect function")
 
-    def import_credentials(self, credentials: OpenStackProject):
-        self.authentication.import_authentication_from_project(credentials)
+    def import_credentials(self, authentication: Authentication):
+        self.authentication.import_authentication(authentication)
 
     def ask_credentials(self):
         self.authentication.ask_credentials()
