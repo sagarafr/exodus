1232db4bb21c0c13d7c9d46bed299e48893f8621
Marian Gappa <marian.gappa@corp.ovh.com>
2017-04-24 12:58:18 +0200

    fix(migration): Change nics_id to nics

--- migration/launch_instance.py
+++ migration/launch_instance.py
@@ -1,10 +1,10 @@
 from connections.nova_connection import NovaConnection
 from utils.find_flavors import find_flavors
+from utils.get_ids import get_image_from_nova
 
 
 def launch_instance(nova_connection: NovaConnection, instance_name: str,
-                    snapshot_name: str, flavor: str, network: str):
-    print(nova_connection.authentication, network)
-    image_uuid = nova_connection.connection.glance.find_image(snapshot_name)
+                    snapshot_name: str, flavor: str, network: list):
+    image = get_image_from_nova(nova_connection, snapshot_name)[0]
     flavor_uuid = find_flavors(nova_connection, flavor)[0]
-    nova_connection.connection.servers.create(instance_name, image_uuid, flavor_uuid, nics_id=network)
+    nova_connection.connection.servers.create(instance_name, image, flavor_uuid, nics=network)
