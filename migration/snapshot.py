6cbb95b964c5eb44fb822f2c980f637705785b65
Marian Gappa <marian.gappa@corp.ovh.com>
2017-05-02 19:21:18 +0200

    chore(TODO and INSTALL): Add instructions

--- migration/snapshot.py
+++ migration/snapshot.py
@@ -9,13 +9,19 @@ def make_snapshot(nova_connection: NovaConnection, server_name: str, snapshot_na
 
     # TODO make a user choice here
     server_uuid = server_id[0]
+    make_snapshot_from_uuid(nova_connection, server_uuid, snapshot_name)
+
+
+def make_snapshot_from_uuid(nova_connection: NovaConnection, server_uuid: str, snapshot_name: str):
     # Can raise a nova_exception.Conflict if there have multiple snap from the same server name
     try:
         snapshot_image_uuid = nova_connection.connection.servers.create_image(server_uuid, snapshot_name)
-    except:
+    except nova_connection.Conflict:
         raise
 
     # TODO make a thread ? a signal ? sleep call ?
+    # TODO make a timeout mode
+    # TODO (test) make multi read and multi write
     is_active = False
     while not is_active:
         image_info = dict(nova_connection.connection.glance.find_image(snapshot_image_uuid).to_dict())
