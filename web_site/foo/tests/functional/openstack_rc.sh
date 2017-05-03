1646737cdaefa2990da6206873466e31b3be5d43
Marian Gappa <marian.gappa@corp.ovh.com>
2017-05-03 20:08:16 +0200

    chore(cleaning): Remove useless files

--- web_site/foo/tests/functional/openstack_rc.sh
+++
@@ -1,8 +0,0 @@
-export OS_USERNAME=username
-export OS_PASSWORD=password
-export OS_TENANT_NAME=projectName
-export OS_AUTH_URL=https://identityHost:portNumber/v2.0
-# The following lines can be omitted
-export OS_TENANT_ID=tenantIDString
-export OS_REGION_NAME=regionName
-export OS_CACERT=/path/to/cacertFile
