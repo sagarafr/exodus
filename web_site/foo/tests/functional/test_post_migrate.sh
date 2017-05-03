ab4019c6481cca25104d7511034e19ece4a2aee3
Marian Gappa <marian.gappa@corp.ovh.com>
2017-05-03 19:48:07 +0200

    test(api): Add test for post v6/migrate

--- web_site/foo/tests/functional/test_post_migrate.sh
+++ web_site/foo/tests/functional/test_post_migrate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 5 ]; then
 exit 42
 fi
 
@@ -8,17 +8,28 @@ ADDRESS_PORT=$1
 USERNAME=$2
 PASSWORD=$3
 AUTH_URL=$4
+TYPE=$5
 
 generate_post_data()
 {
-  cat <<EOF
+cat << EOF
 {
-"username": "$USERNAME",
-"password": "$PASSWORD",
-"auth_url": "$AUTH_URL"
+"from": {"username": "$USERNAME", "password": "$PASSWORD", "auth_url": "$AUTH_URL", "type": "$TYPE"},
+"to": {"username": "$USERNAME", "password": "$PASSWORD", "auth_url": "$AUTH_URL", "type": "$TYPE"}
 }
 EOF
 }
 
+generate_post_error()
+{
+cat << EOF
+{
+"from": {"username": "$USERNAME", "password": "$PASSWORD", "auth_url": "$AUTH_URL", "type": "$TYPE"},
+"a": {"username": "$USERNAME", "password": "$PASSWORD", "auth_url": "$AUTH_URL", "type": "$TYPE"},
+"to": {"username": "$USERNAME", "password": "$PASSWORD", "auth_url": "$AUTH_URL", "type": "$TYPE"}
+}
+EOF
+}
 
 curl -X POST ${ADDRESS_PORT}/v6/migrate -H 'Content-Type: application/json' -d "$(generate_post_data)"
+curl -X POST ${ADDRESS_PORT}/v6/migrate -H 'Content-Type: application/json' -d "$(generate_post_error)"
