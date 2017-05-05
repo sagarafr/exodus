5ffcba29b1b4394147a0f86324dec114b3c15bf4
Marian Gappa <marian.gappa@corp.ovh.com>
2017-05-05 15:56:55 +0200

    feat(migration): Add some exceptions and change make_snapshot function make_snapshot function check now different status

--- migration/launch_instance.py
+++ migration/launch_instance.py
@@ -3,7 +3,6 @@ from utils.get_ids import get_image_from_nova
 from connections.nova_connection import NovaConnection
 
 
-# TODO make this more secure with some try except
 def launch_instance(nova_connection: NovaConnection, instance_name: str,
                     snapshot_name: str, flavor: str, network: list):
     """
@@ -15,7 +14,15 @@ def launch_instance(nova_connection: NovaConnection, instance_name: str,
     :param snapshot_name: str that represent the name of the snapshot
     :param flavor: str that represent the name of the flavor
     :param network: list content the nics representation of the network
+    :raise: ValueError if snapshot_name or flavor not found
     """
-    image = get_image_from_nova(nova_connection, snapshot_name)[0]
-    flavor_uuid = find_flavors(nova_connection, flavor)[0]
+    image, flavor_uuid = None, None
+    try:
+        image = get_image_from_nova(nova_connection, snapshot_name)[0]
+    except IndexError as index:
+        raise ValueError(snapshot_name + " snapshot not found")
+    try:
+        flavor_uuid = find_flavors(nova_connection, flavor)[0]
+    except IndexError as index:
+        raise ValueError(flavor + " flavor not found")
     nova_connection.connection.servers.create(instance_name, image, flavor_uuid, nics=network)
