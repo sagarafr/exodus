3f377f68a07de3dae89ae086437cc0c8351a8871
Marian Gappa <marian.gappa@corp.ovh.com>
2017-06-01 18:52:15 +0200

    test(volume): Test all volume disk migration process

--- tests/test_volume.py
+++ tests/test_volume.py
@@ -1,13 +1,14 @@
 from getpass import getpass
+from datetime import datetime
 from connections.nova_connection import NovaConnection
 from connections.cinder_connection import CinderConnection
 from connections.glance_connection import GlanceConnection
 from utils.detach_volume import detach_all_volumes
 from utils.attach_a_volume import attach_volumes
+from utils.get_ids import get_snapshot_id_from_glance
 from migration.snapshot import make_volume_snapshot
 from migration.snapshot import make_hard_disk_snapshot
 from migration.migration import migration
-from datetime import datetime
 
 
 def info_volume():
@@ -24,13 +25,17 @@ def info_volume():
 
 
 def migrate():
-    creds = {"auth_url": "https://auth.cloud.ovh.net/v3",
-             "user_domain_name": "default",
-             "username": input("Username:" ),
-             "password": getpass(),
-             "version": "2",
-             "region_name": "GRA3"}
-    ovh_cinder_connection = CinderConnection(**creds)
+    creds_cinder = {"auth_url": "https://auth.cloud.ovh.net/v3",
+                    "user_domain_name": "default",
+                    "username": input("Username: "),
+                    "password": getpass(),
+                    "version": "2",
+                    "region_name": "GRA3"}
+    ovh_cinder_connection = CinderConnection(**creds_cinder)
+    creds_cinder_dest = {"authentication": ovh_cinder_connection.authentication,
+                         "version": 2,
+                         "region_name": "BHS3"}
+    ovh_cinder_connection_dest = CinderConnection(**creds_cinder_dest)
     creds_src = {"authentication": ovh_cinder_connection.authentication,
                  "version": 2,
                  "region_name": "GRA3"}
@@ -39,10 +44,7 @@ def migrate():
                   "version": 2,
                   "region_name": "BHS3"}
     ovh_glance_dest_connection = GlanceConnection(**creds_dest)
-    creds_nova = {"auth_url": "https://auth.cloud.ovh.net/v3",
-                  "user_domain_name": "default",
-                  "username": input("Username: "),
-                  "password": getpass(),
+    creds_nova = {"authentication": ovh_glance_dest_connection.authentication,
                   "version": 2,
                   "region_name": "GRA3"}
     ovh_nova_connection = NovaConnection(**creds_nova)
@@ -50,6 +52,8 @@ def migrate():
         snapshot_name = volume + datetime.now().isoformat()
         make_hard_disk_snapshot(ovh_cinder_connection, ovh_glance_src_connection, ovh_nova_connection, volume, snapshot_name)
         migration(ovh_glance_src_connection, ovh_glance_dest_connection, snapshot_name, snapshot_name)
+        uuid_snap = get_snapshot_id_from_glance(ovh_glance_dest_connection, snapshot_name)[0]
+        ovh_cinder_connection_dest.connection.volumes.create(10, imageRef=uuid_snap)
 
 
 def test_volume():
